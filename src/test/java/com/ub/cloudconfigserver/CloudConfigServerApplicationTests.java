8fbde7f2f2b2bd5a3bffe273b838cd1eb5b7996a
Kartik <kartik15nov@gmail.com>
2021-01-20 15:05:36 +0530

    Added Cloud config server with basic configuration.

---
+++ src/test/java/com/ub/cloudconfigserver/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ub.cloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
